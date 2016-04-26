f3a49e84000288a5a824a8455665ee430a6d9b8d
Martin Flucka <martin.flucka@gmail.com>
2016-04-26 19:07:53 +0200

    fix: add dot at the end of all feedback lines

--- lib/src/feedback.dart
+++ lib/src/feedback.dart
@@ -4,27 +4,27 @@ import 'package:xcvbnm/src/scoring.dart' as scoring;
 
 class Feedback {
   static final String generalSuggestion = 'Use a few words, avoid common phrases. '
-      'No need for symbols, digits, or uppercase letters';
+      'No need for symbols, digits, or uppercase letters.';
   static final String spatialSingleTurnSuggestion = 'Straight rows of keys are easy to guess. '
-      'Use a longer keyboard pattern with more turns';
+      'Use a longer keyboard pattern with more turns.';
   static final String spatialMultiTurnSuggestion = 'Short keyboard patterns are easy to guess. '
-      'Use a longer keyboard pattern with more turns';
+      'Use a longer keyboard pattern with more turns.';
   static final String repeatSingleCharSuggestion = 'Repeats like \'aaa\' are easy to guess. '
-      'Avoid repeated words and characters';
+      'Avoid repeated words and characters.';
   static final String repeatMultiCharSuggestion =
       'Repeats like \'abcabcabc\' are only slightly harder to guess than \'abc\'. '
-      'Avoid repeated words and characters';
+      'Avoid repeated words and characters.';
   static final String sequenceSuggestion = 'Sequences like abc or 6543 are easy to guess. '
-      'Avoid sequences';
+      'Avoid sequences.';
   static final String recentYearSuggestion = 'Recent years are easy to guess. '
-      'Avoid recent years and years that are associated with you';
+      'Avoid recent years and years that are associated with you.';
   static final String dateSuggestion = 'Dates are often easy to guess. '
-      'Avoid dates and years that are associated with you';
-  static final String top10PasswordSuggestion = 'This is a top-10 common password';
-  static final String top100PasswordSuggestion = 'This is a top-100 common password';
-  static final String commonPasswordSuggestion = 'This is a very common password';
-  static final String englishWordSuggestion = 'A word by itself is easy to guess';
-  static final String nameSuggestion = 'Common names and surnames are easy to guess';
+      'Avoid dates and years that are associated with you.';
+  static final String top10PasswordSuggestion = 'This is a top-10 common password.';
+  static final String top100PasswordSuggestion = 'This is a top-100 common password.';
+  static final String commonPasswordSuggestion = 'This is a very common password.';
+  static final String englishWordSuggestion = 'A word by itself is easy to guess.';
+  static final String nameSuggestion = 'Common names and surnames are easy to guess.';
 
   String suggestion = generalSuggestion;
 
